fe7baf756bf5f22857f3e877c56c586d79bacb38
Phan Van Hoa Thuan <thuankg1207@gmail.com>
2024-06-17 10:16:48 +0700

    Test

--- src/ProjectManagement.java
+++ src/ProjectManagement.java
@@ -396,7 +396,7 @@ public class ProjectManagement {
 	}
 
 	public void updateProject () {
-		System.out.println("Testing update project 2nd times");
+		System.out.println("Thuan Phan test commit");
 	}
 
 	/* Public methods end */
